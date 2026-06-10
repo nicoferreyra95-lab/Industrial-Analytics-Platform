f0d746b3e2f7ed687706e73414715cea71df6943
nicoferreyra95-lab <nicolase.ferreyra95@gmail.com>
2026-06-10 15:45:29 -0300

    10-06  csv cargados al SQL

--- sql/Create_tables.sql
+++ sql/Create_tables.sql
@@ -4,3 +4,5 @@ CREATE TABLE products ( Product text PRIMARY KEY, Flavor text, Size int, min_bat
 CREATE TABLE Line_Productivity (Date DATE, Product_id text REFERENCES products(Product), Batch int PRIMARY KEY, Operator text, Start_time time, End_time time);
 
 CREATE TABLE Line_Downtime (Batch int REFERENCES Line_Productivity(Batch), Downtime_factor int REFERENCES Downtime_Factors(Factor_id), Time_consumed_min int, PRIMARY KEY (Batch, Downtime_factor))
+
+select * from Downtime_factors
